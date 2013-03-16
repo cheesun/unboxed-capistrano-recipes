f9fb83542de1696338bc5d58d37ef7f15c10f8c6
Attila Györffy <attila@attilagyorffy.com>
2013-03-16 17:05:21 +0100

    Initial commit, with a deploy:revision task

---
+++ lib/unboxed/capistrano/extensions.rb
@@ -0,0 +1,9 @@
+require "unboxed/capistrano/extensions/version"
+
+module Unboxed
+  module Capistrano
+    module Extensions
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/unboxed/capistrano/extensions/version.rb
@@ -0,0 +1,7 @@
+module Unboxed
+  module Capistrano
+    module Extensions
+      VERSION = "0.0.1"
+    end
+  end
+end
