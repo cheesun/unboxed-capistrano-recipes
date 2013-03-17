ff5f398e7a30f4251b7019d4659bf98635bd8a69
Attila Györffy <attila@attilagyorffy.com>
2013-03-17 19:42:10 +0100

    Add a Passenger version of deploy:start, deploy:stop and deploy:restart

---
+++ lib/unboxed/capistrano/recipes/deploy/passenger.rb
@@ -0,0 +1,10 @@
+Unboxed::Capistrano::Recipes::Configuration.load do
+  namespace :deploy do
+    task :start do ; end
+    task :stop do ; end
+    task :restart, :roles => :app, :except => { :no_release => true } do
+      run "touch #{File.join(current_path, 'tmp' ,'restart.txt')}"
+    end
+  end
+end
+
