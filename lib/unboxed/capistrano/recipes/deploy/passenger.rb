81e7578c8437e7c6b6e6c78df81d4719bc20ad2b
Attila Györffy <attila@attilagyorffy.com>
2013-03-17 20:19:53 +0100

    Add descriptions to the Passenger deployment tasks.
    To make it clear that the application is deployed via Passenger when listing Capistrano tasks.
    
    `cap -T`

--- lib/unboxed/capistrano/recipes/deploy/passenger.rb
+++ lib/unboxed/capistrano/recipes/deploy/passenger.rb
@@ -1,7 +1,10 @@
 Unboxed::Capistrano::Recipes::Configuration.load do
   namespace :deploy do
+    desc "Start the application via Passenger (has no effect)"
     task :start do ; end
+    desc "Stop the application via Passenger (has no effect)"
     task :stop do ; end
+    desc "Restart the Passenger application server"
     task :restart, :roles => :app, :except => { :no_release => true } do
       run "touch #{File.join(current_path, 'tmp' ,'restart.txt')}"
     end
